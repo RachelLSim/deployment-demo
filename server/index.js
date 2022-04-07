425561947cfa97beafb95f0dadbadf0530f4997b
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-04-07 13:16:42 -0600

    rollbar

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '377f14c9e9d8471bac00244f0a049462',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
